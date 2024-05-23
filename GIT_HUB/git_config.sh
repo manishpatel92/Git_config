a2ffc981ef038be02494208098b7d0cec7c0d70a
Manish Patel <107122411+manishpatel92@users.noreply.github.com>
2024-05-24 00:49:30 +0530

    git_config.sh

--- GIT_HUB/git_config.sh
+++ GIT_HUB/git_config.sh
@@ -2,11 +2,11 @@ sudo yum update -y
 sudo yum install git -y
 git config --global user.name "manishpatel92"
 git config --global user.email "manish080194@gmail.com"
+---------------------------------------------------------
 ssh-keygen -t ed25519 -C "manish080194@gmail.com"
 
 eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/id_ed25519
 
 cat ~/.ssh/id_ed25519.pub
-ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHDTKf2vPqKYs+f39/hkBT98j0G1ZaWF8qGReY1TPaeZ manish080194@gmail.com
-
+=====================================================
