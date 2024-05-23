ca692dd24cb6735b0de88d946bbd73610169c5c7
Manish Patel <107122411+manishpatel92@users.noreply.github.com>
2024-05-24 00:51:35 +0530

    Update git_config.sh

--- GIT_HUB/git_config.sh
+++ GIT_HUB/git_config.sh
@@ -11,3 +11,11 @@ ssh-add ~/.ssh/id_ed25519
 cat ~/.ssh/id_ed25519.pub
 =====================================================
 111111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111111
+--------------------------------------------------
+git push https://1111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1111@github.com/manishpatel92/Git_config.git
+
+git remote add origin https://github.com/manishpatel92/Git_config.git
+
+git push https://1111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11111@github.com/manishpatel92/New_Relic.git
+
+
